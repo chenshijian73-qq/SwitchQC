91c66d2f058203e12ec87b7f7abf8708c9dd673e
chenshijian <chenshijian@coidng.net>
2023-05-24 22:17:57 +0800

    feat: getFiles from local

---
+++ frontend/wailsjs/go/models.ts
@@ -0,0 +1,19 @@
+export namespace main {
+	
+	export class QuickCmdFile {
+	    name: string;
+	    content: string;
+	
+	    static createFrom(source: any = {}) {
+	        return new QuickCmdFile(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.name = source["name"];
+	        this.content = source["content"];
+	    }
+	}
+
+}
+
