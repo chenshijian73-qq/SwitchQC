be499fecda164d2caf32ce5c697b4dd4d280a078
chenchenshi <15815127500@163.com>
2023-06-21 15:26:53 +0800

    feat: 前端基本页面开发

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,33 +1,110 @@
 <template>
-  <div id="app" style="display: flex; height: 100%;">
-    <FileList @file-selected="selectFile" style="width: 15%; height: 100%; overflow-y: scroll; border-right: 1px solid #eec5c5;" />
-    <FileContent :selected-file="selectedFile" style="width: 80%; height: 100%; padding-left: 10px; overflow-y: scroll;" />
+  <div class="app">
+    <div class="header">
+      <a-button type="primary" v-if="showNav" @click="showNav = false">关闭导航栏</a-button>
+      <a-button type="primary" v-else @click="showNav = true">打开导航栏</a-button>
+      <a-button type="primary" @click="addFile">添加文件</a-button>
+    </div>
+    <div class="main">
+      <div class="nav" v-if="showNav">
+        <ul>
+          <li v-for="(file, index) in qcFiles" :key="index" @click="selectFile(file, $event)" @contextmenu.prevent>
+            <span class="name">{{ file.name }}</span>
+            <a-switch v-model="file.enabled" />
+          </li>
+        </ul>
+      </div>
+      <div class="content">
+        <a-textarea v-model="selectedFile.content" :disabled="!selectedFile.enabled" @input="saveFile" />
+      </div>
+    </div>
   </div>
 </template>
-<style>
-  @import 'ant-design-vue/dist/reset.css';
-</style>
+
 <script>
-import { ref } from 'vue';
-import FileList from './components/FilesList.vue';
-import FileContent from './components/FileContent.vue';
-
 export default {
-  components: {
-    FileList,
-    FileContent,
-  },
-  setup() {
-    const selectedFile = ref(null);
-
-    const selectFile = (file) => {
-      selectedFile.value = file;
-    };
-
+  data() {
     return {
-      selectedFile,
-      selectFile,
+      showNav: true,
+      qcFiles: [
+        { name: 'file1', content: 'This is file1 content.', enabled: true },
+        { name: 'file2', content: 'This is file2 content.', enabled: false },
+        { name: 'file3', content: 'This is file3 content.', enabled: true },
+      ],
+      selectedFile: {},
+      contextMenuVisible: false,
     };
+  },
+  methods: {
+    addFile() {
+      // 添加文件的逻辑
+    },
+    selectFile(file, event) {
+      this.selectedFile = file;
+      if (event.button === 2) {
+        this.showContextMenu(file, event);
+      }
+    },
+    editFile(file) {
+      // 编辑文件信息的逻辑
+      console.log('编辑文件信息', file);
+    },
+    removeFile(file) {
+      // 移动文件到回收站的逻辑
+      console.log('移动到回收站', file);
+    },
+    saveFile() {
+      // 保存文件的逻辑
+    },
   },
 };
 </script>
+
+<style scoped>
+.app {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+}
+
+.header {
+  display: flex;
+  justify-content: space-between;
+  padding: 16px;
+}
+
+.main {
+  display: flex;
+  flex: 1;
+  overflow: hidden;
+}
+
+.nav {
+  width: 200px;
+  border-right: 1px solid #e8e8e8;
+  overflow: auto;
+}
+
+ul {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+
+li {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 8px 16px;
+  cursor: pointer;
+}
+
+.name {
+  margin-left: 8px;
+}
+
+.content {
+  flex: 1;
+  padding: 16px;
+}
+</style>
